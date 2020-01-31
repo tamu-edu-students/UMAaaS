5779a6d9ac21f69abd43c3229bc6227045ee0422
Ben Jirka <bjirka@tamu.edu>
2020-01-31 20:51:04 +0000

    added User edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,19 @@ class UsersController < ApplicationController
   #belongs_to :program, optional: true
   
   def index
-    puts "HERE WE GO!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
       @users = User.all
   end
   
   def edit
+        @user = User.find params[:id]
+        @programs = Program.all
+  end
+  
+  def update
+    @user = User.find params[:id]
+    @user.update_attributes(:programs_id => params[:program_id])
+    flash[:notice] = "#{@user.name} was successfully updated."
+    redirect_to users_path
   end
   
   def destroy
