c6656d401d6285207dae32c529bbdf6d0ad9445e
Ben Jirka <bjirka@tamu.edu>
2020-02-19 07:44:00 +0000

    Experiences uses partials now, sorting works, average rating works

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -5,17 +5,26 @@ class ExperiencesController < ApplicationController
     
     def create
         Experience.create(:experience => params[:experience][:experience], :rating => params[:experience][:rating], :user_id => session[:user], :program_id => params[:id])
-        #puts "THE RATING IS: " + params[:experience][:rating].to_s
         redirect_to portal_path(params[:id])
     end
     
     
     def create_comment
-        puts "START INSERT HERE"
         ExperienceComment.create(:comment => params[:commentText], :rating => params[:rating], :user_id => session[:user], :experience_id => params[:experienceId])
-        puts "WOULD INSERT HERE"
+        @experience = Experience.left_outer_joins(:user).select("experiences.*,users.name as user_name").where(experiences: {id: params[:experienceId]}).first
+        
+        @experience.comments = ExperienceComment.left_outer_joins(:user).select("experience_comments.*,users.name as user_name").where(experience_id: params[:experienceId]).order(created_at: :desc)
+        
+        rating_sum = ExperienceComment.where(experience_id: params[:experienceId]).group(:experience_id).sum(:rating).values[0]
+        rating_sum += @experience.rating #add the original rating
+        rating_count = ExperienceComment.where(experience_id: params[:experienceId]).where.not(rating: nil).count(:id)
+        rating_count +=1 #add 1 for the original rating
+        @experience.average_rating = (rating_sum.to_f / rating_count).round(1)
+        
+        @experienceDivId = "portal-experience-wrapper-" + params[:experienceId]
+        
         respond_to do |format|
-            format.json { render json: { results: params[:experienceId] } }
+            format.js {}
         end
     end
 end
