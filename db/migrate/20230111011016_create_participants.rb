53b68cc26fbaaa3ca41a91fc1dbc67e1fac5f1a3
Abhishek <abhimore74@gmail.com>
2023-01-12 14:21:38 +0000

    removed duplicate email

--- db/migrate/20230111011016_create_participants.rb
+++ db/migrate/20230111011016_create_participants.rb
@@ -2,8 +2,6 @@ class CreateParticipants < ActiveRecord::Migration[6.0]
   def change
     create_table :participants do |t|
       t.boolean :is_faculty
-      t.string :email
-
       t.timestamps
     end
   end
