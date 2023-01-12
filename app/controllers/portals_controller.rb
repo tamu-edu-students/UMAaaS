71bbada79215929ceeceddfffe29ce2d29ffb6ee
Abhishek <abhimore74@gmail.com>
2023-01-12 12:29:43 +0000

    added middleware auth for experience and tips + refactored views

--- app/controllers/portals_controller.rb
+++ app/controllers/portals_controller.rb
@@ -1,4 +1,12 @@
 class PortalsController < ApplicationController
+    helper_method :has_program?,
+
+    def has_program?
+        puts params
+        participant = Participant.find_by(email: current_user.email, program_id: params[:id])
+        return !participant.nil?
+    end
+
     def index
         @programs = Program.where(disabled: false)
         
@@ -43,6 +51,7 @@ class PortalsController < ApplicationController
     def view
         #get this specific program
         @program = Program.find params[:id]
+
         
         # get list of all programs to display in drop down list for switching between
         @programs = Program.where(disabled: false)
