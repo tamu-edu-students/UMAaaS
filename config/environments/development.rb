25f22382fdceb1c6cdbcd60e5e53d5e076276f96
Abhishek <abhimore74@gmail.com>
2023-02-07 00:09:24 +0000

    env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.hosts << ENV["AWS_HOST"]
+  #config.hosts << "999663bc4eee4a0cb47c75a9aaa9268b.vfs.cloud9.us-east-1.amazonaws.com"
   #config.hosts << "389763a4bd934fd2b31f81763dda423c.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
