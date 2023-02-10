6a2cc8bb20bf180af77d5d9984ba121d6c95d27a
EC2 Default User <ec2-user@ip-172-31-88-177.ec2.internal>
2023-02-10 05:02:32 +0000

    Added iteration-0 report

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.hosts << ENV['AWS_HOST']
+  config.hosts << "e0f13ef4e3584e999773685843a8fad8.vfs.cloud9.us-east-1.amazonaws.com"
   # config.hosts << "999663bc4eee4a0cb47c75a9aaa9268b.vfs.cloud9.us-east-1.amazonaws.com"
   # config.hosts << "389763a4bd934fd2b31f81763dda423c.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
