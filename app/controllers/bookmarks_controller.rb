d0e74c2ba364a8ffbfba11aacae05941718a08da
EC2 Default User <ec2-user@ip-172-31-88-177.ec2.internal>
2023-02-25 17:57:19 +0000

    Created-bookmarks-page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,9 +9,15 @@ class BookmarksController < ApplicationController
     def view
       @user = User.find params[:id]
       @bookmarks = Bookmark.left_outer_joins(:user).select("bookmarks.*").where(users: { id: params[:id]})
-      @experiences = 
-    end   
-    
+      @experiences = Array.new
+      @titles = Array.new
+      @bookmarks.each do |item|
+        @experiences.append(item.experience_id)
+        @titles.append(Experience.find(item.experience_id))
+      end
+    end
     def delete
+    end
+    def index
     end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,4 @@
 class Bookmark < ApplicationRecord
     belongs_to :user
     has_one :experience
-    
 end
