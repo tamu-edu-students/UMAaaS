41ecc6ec1c1d98e38cd4aadeaf1071d5e8478f53
aketkar18 <anujketkar1118@gmail.com>
2023-02-28 23:08:22 +0000

    trying out false migration

--- app/controllers/portals_controller.rb
+++ app/controllers/portals_controller.rb
@@ -136,7 +136,8 @@ class PortalsController < ApplicationController
 
   
     # get all experiences for this program
-    @experiences = Experience.left_outer_joins(:user).select('experiences.*,users.name as user_name').where(experiences: { program_id: params[:id] }).where(users: { banned: false })
+    @experiences = Experience.left_outer_joins(:user).select('experiences.*,users.name as user_name').where(experiences: { program_id: params[:id] }).where(users: { banned: false }).where('users.banned IS NULL OR users.banned = ?', false)
+    #@experiences = Experience.left_outer_joins(:user).select('experiences.*,users.name as user_name').where(experiences: { program_id: params[:id] }).where(users: { banned: false })
   # @experiences = Experience.left_outer_joins(:user).left_outer_joins(:yelp_location).select('experiences.*,users.name as user_name,yelp_locations.name as yelp_name, yelp_locations.address as yelp_address, yelp_locations.alias as yelp_alias, yelp_locations.url as yelp_url, yelp_locations.image_url as yelp_image_url, yelp_locations.rating as yelp_rating').where(experiences: { program_id: params[:id] }).where(users: { banned: false }).order(rating: :desc)
 
 
@@ -213,7 +214,6 @@ puts("Hello")
       @experiences = @experiences.sort_by(&:rating)
     when 'avg_rating_asc'
       @experiences = @experiences.sort_by(&:average_rating)
-    when 'date'
       @experiences = @experiences.sort_by(&:created_at).reverse!
     when 'comments'
       @experiences = @experiences.sort_by(&:totalComments).reverse!
