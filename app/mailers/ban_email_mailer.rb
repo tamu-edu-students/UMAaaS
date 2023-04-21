88346bb2de7f2d40d6ff3b74669f57fc0ef7046d
EC2 Default User <ec2-user@ip-172-31-78-139.ec2.internal>
2023-04-21 01:39:30 +0000

    add new column to save the ban reason

--- app/mailers/ban_email_mailer.rb
+++ app/mailers/ban_email_mailer.rb
@@ -2,8 +2,9 @@
 class BanEmailMailer < ApplicationMailer
   default from: ENV["GMAIL_USERNAME"]
 
-  def banned(user)
+  def banned(user, ban_reason)
     @user = user
-    mail(to: @user.email, subject: 'You have been banned')
+    @ban_reason = ban_reason
+    mail(to: @user.email, subject: 'You Have Been Banned')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_23_033352) do
+ActiveRecord::Schema.define(version: 2023_04_20_052738) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -150,6 +150,7 @@ ActiveRecord::Schema.define(version: 2023_03_23_033352) do
     t.integer "program_id"
     t.boolean "admin", default: false
     t.boolean "banned", default: false
+    t.string "ban_reason"
     t.index ["program_id"], name: "index_users_on_program_id"
   end
 
