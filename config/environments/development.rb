f4076f3c4651df707b6ba05d37157a82f91602b2
EC2 Default User <ec2-user@ip-172-31-78-139.ec2.internal>
2023-04-26 15:48:51 +0000

    add to production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: '2dbaedf1c0ce4258818f3f8ba7f64d58.vfs.cloud9.us-east-1.amazonaws.com' }
+  config.action_mailer.default_url_options = { host: 'tripagvisor2.herokuapp.com' }
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
